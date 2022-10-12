08867219a2ea2cdb8c67ba93b4e67e729e43b610
Eduardo Ban <eduardoban@gmail.com>
2022-10-12 20:47:32 -0300

    datos log

--- build/server.js
+++ build/server.js
@@ -1,35 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//
-// example-tcp.ts
-//
-// Run: npx ts-node ./example/example-tcp.ts
-//
-// Expected output:
-//   pocket-sockets: TCP example
-//   Server: listening...
-//
 require('dotenv').config();
-//import { prototype } from "events";
 const pocket_sockets_1 = require("pocket-sockets");
-console.log("pocket-sockets: TCP example");
 const serverOptions = {
     host: "localhost",
     port: parseInt(process.env.App_Port, 10) || 3000
 };
 const server = new pocket_sockets_1.TCPServer(serverOptions);
 server.listen();
-console.log("Server: puerto .env: ", process.env.App_Port);
 console.log("Servidor escuchando puerto: " + serverOptions.port);
 server.onConnection((client) => {
     var _a;
-    console.log("Servidor conexion aceptada IP: " + ((_a = client.getRemoteAddress()) === null || _a === void 0 ? void 0 : _a.toString()));
+    console.log("Conexion aceptada IP: " + ((_a = client.getRemoteAddress()) === null || _a === void 0 ? void 0 : _a.toString()));
     client.onData((data) => {
-        console.log("Server: incoming client data", data.toString());
-        client.sendString("This is server: received!");
+        console.log("Recibiendo datos: ", data.toString());
+        client.sendString("Datos resividos");
     });
     client.onClose(() => {
-        console.log("Server: client connection closed");
+        console.log("Servidor: cerrado por el cliente");
         server.close();
     });
 });
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,23 @@
 require('dotenv').config();
 import {TCPServer, Client} from "pocket-sockets";
  
-console.log("pocket-sockets: TCP example");
 const serverOptions = {
     host: "localhost",
     port: parseInt(<string>process.env.App_Port, 10) || 3000 
 };
+
 const server = new TCPServer(serverOptions);
 server.listen();
-console.log("Server: puerto .env: ",process.env.App_Port );
 console.log("Servidor escuchando puerto: "+ serverOptions.port);
 
 server.onConnection( (client: Client) => {
-    console.log("Servidor conexion aceptada IP: " + client.getRemoteAddress()?.toString());
+    console.log("Conexion aceptada IP: " + client.getRemoteAddress()?.toString());
     client.onData( (data: Buffer) => {
-        console.log("Server: incoming client data", data.toString());
-        client.sendString("This is server: received!");
+        console.log("Recibiendo datos: ", data.toString());
+        client.sendString("Datos recividos");
     });
     client.onClose( () => {
-        console.log("Server: client connection closed");
+        console.log("Servidor: cerrado por el cliente");
         server.close();
     });
 });
